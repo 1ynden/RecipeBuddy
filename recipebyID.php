945cdcc59cff3f5778d99e23bbbd7e6eced9a547
lynden <millingtonlyn00@gmail.com>
2021-12-06 00:29:43 -0500

    Update recipebyID.php
    => is evidently incorrect syntax.

--- recipebyID.php
+++ recipebyID.php
@@ -27,14 +27,14 @@ if(isset($_POST["searchID"]))
     if(empty($row_search)){
 			echo "No Recipes found, please try again";
 		} else{
-			$output['name'] => $row_search[2]; //name
-			$output['desc'] => $row_search[3]; //description
-			$output['tags'] => $row_search[4]; //tags
-			$output['time'] => $row_search[5]; //time needed
-			$output['serv'] => $row_search[7]; //servings
-			$output['diff'] => $row_search[8]; //difficulty
-			$output['imag'] => $row_search[9]; //picture
-			$output['cook'] => $row_search[10]; //times cooked
+			$output['name'] = $row_search[2]; //name
+			$output['desc'] = $row_search[3]; //description
+			$output['tags'] = $row_search[4]; //tags
+			$output['time'] = $row_search[5]; //time needed
+			$output['serv'] = $row_search[7]; //servings
+			$output['diff'] = $row_search[8]; //difficulty
+			$output['imag'] = $row_search[9]; //picture
+			$output['cook'] = $row_search[10]; //times cooked
       print json_encode($output);
     }
 
