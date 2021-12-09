492a7fb2f1091ece73765e30e120bc937ae9f59c
lynden <millingtonlyn00@gmail.com>
2021-12-09 09:53:38 -0500

    Returning rating to search

--- getSearch.php
+++ getSearch.php
@@ -16,8 +16,9 @@ $output = array();
 
 if( $result->num_rows > 0 ) {
     while( $row = $result->fetch_assoc() ){
-        $ratingsql = "SELECT SUM(`user_rating`) FROM `recipe_ratings` WHERE `recipe_ID` = "$row['recipe_ID']"";
-        $output[] = array( "recipe_Name" => $row[ "recipe_Name" ], "recipe_Description" => $row[ "recipe_Description" ], "recipe_ID" => $row[ "recipe_ID" ], "time" => $row[ "time" ] );
+        $ratingsql = "SELECT AVG(`user_rating`) FROM `recipe_ratings` WHERE `recipe_ID` = $row['recipe_ID']";
+        $ratings = $con->query( $sql );
+        $output[] = array( "recipe_Name" => $row[ "recipe_Name" ], "recipe_Description" => $row[ "recipe_Description" ], "recipe_ID" => $row[ "recipe_ID" ], "time" => $row[ "time" ], "score" => $ratings );
     }
 }
 $con->close();
