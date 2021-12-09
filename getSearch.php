f33a4f5504ff0ac54a2e61766beb4005c375bb5e
lynden <millingtonlyn00@gmail.com>
2021-12-09 10:10:59 -0500

    Dummy

--- getSearch.php
+++ getSearch.php
@@ -17,7 +17,7 @@ $output = array();
 if( $result->num_rows > 0 ) {
     while( $row = $result->fetch_assoc() ){
         $dummyRating = $row["recipe_ID"];
-        $ratingsql = "SELECT AVG(`user_rating`) FROM `recipe_ratings` WHERE `recipe_ID` = '$dummyRating'";
+        $ratingsql = "SELECT AVG(`user_rating`) FROM `recipe_ratings` WHERE `recipe_ID` = 1";
         $ratings = $con->query( $sql );
         $output[] = array( "recipe_Name" => $row[ "recipe_Name" ], "recipe_Description" => $row[ "recipe_Description" ], "recipe_ID" => $row[ "recipe_ID" ], "time" => $row[ "time" ], "score" => $ratings );
     }
