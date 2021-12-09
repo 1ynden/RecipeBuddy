02eae40458c429e7dd3ebed2986ee1410065b221
lynden <millingtonlyn00@gmail.com>
2021-12-09 09:36:43 -0500

    Test menu change

--- getSearch.php
+++ getSearch.php
@@ -16,6 +16,7 @@ $output = array();
 
 if( $result->num_rows > 0 ) {
     while( $row = $result->fetch_assoc() ){
+        $ratingsql = "SELECT SUM(`user_rating`) FROM `recipe_ratings` WHERE `recipe_ID` = "$row['recipe_ID']"";
         $output[] = array( "recipe_Name" => $row[ "recipe_Name" ], "recipe_Description" => $row[ "recipe_Description" ], "recipe_ID" => $row[ "recipe_ID" ], "time" => $row[ "time" ] );
     }
 }
