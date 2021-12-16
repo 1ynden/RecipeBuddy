f70de44688e6202fc70a64578c83e678c3006ad2
lynden <millingtonlyn00@gmail.com>
2021-12-15 21:50:52 -0500

    No Image.

--- createRecipe.php
+++ createRecipe.php
@@ -1,6 +1,6 @@
 <?php
 
-if(isset($_POST["user"], $_POST["name"], $_POST["desc"], $_POST["ingrs"], $_POST["apps"], $_POST["instr"], $_POST["cat"], $_POST["time"], $_POST["serv"], $_POST["priv"], $_POST["diff"], $_POST["pic"]))
+if(isset($_POST["user"], $_POST["name"], $_POST["desc"], $_POST["ingrs"], $_POST["apps"], $_POST["instr"], $_POST["cat"], $_POST["time"], $_POST["serv"], $_POST["priv"], $_POST["diff"]))
     {
         $user_ID = $_POST["user"];
         $name = $_POST["name"];
@@ -13,7 +13,7 @@ if(isset($_POST["user"], $_POST["name"], $_POST["desc"], $_POST["ingrs"], $_POST
         $servings = $_POST["serv"];
         $private = $_POST["priv"];
         $difficulty = $_POST["diff"];
-        $picture = $_POST["pic"];
+        //$picture = $_POST["pic"];
         $times_cooked = "0";
 
 		$hostName = "localhost";
@@ -30,7 +30,7 @@ if(isset($_POST["user"], $_POST["name"], $_POST["desc"], $_POST["ingrs"], $_POST
 			die("Connection failed: " .mysqli_connect_error());
 		}
 
-      $query = "INSERT INTO `recipe` (`user_ID`, `recipe_ID`, `recipe_Name`, `recipe_Description`, `category`, `time`, `private_recipe`, `servings`, `difficulty`, `picture`, `times_cooked`, `ingredients`, `appliances`) VALUES ('$user_ID', NULL, '$name', '$desc', '$category', '$time', '$private', '$servings', '$difficulty', '$picture', '$times_cooked', '$ingrs', '$apps')";
+      $query = "INSERT INTO `recipe` (`user_ID`, `recipe_ID`, `recipe_Name`, `recipe_Description`, `category`, `time`, `private_recipe`, `servings`, `difficulty`, `picture`, `times_cooked`, `ingredients`, `appliances`) VALUES ('$user_ID', NULL, '$name', '$desc', '$category', '$time', '$private', '$servings', '$difficulty', 'N/A', '$times_cooked', '$ingrs', '$apps')";
 	    $result = mysqli_query($con, $query);
       echo $result[ "recipe_ID" ];
 	}
