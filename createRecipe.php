3e5a9213fb0cdb782b7529a1588a243646f0e73e
lynden <millingtonlyn00@gmail.com>
2021-12-15 21:28:53 -0500

    AJAX > $.post

--- createRecipe.php
+++ createRecipe.php
@@ -32,6 +32,7 @@ if(isset($_POST["user"], $_POST["name"], $_POST["desc"], $_POST["ingrs"], $_POST
 
       $query = "INSERT INTO `recipe` (`user_ID`, `recipe_ID`, `recipe_Name`, `recipe_Description`, `category`, `time`, `private_recipe`, `servings`, `difficulty`, `picture`, `times_cooked`, `ingredients`, `appliances`) VALUES ('$user_ID', NULL, '$name', '$desc', '$category', '$time', '$private', '$servings', '$difficulty', '$picture', '$times_cooked', '$ingrs', '$apps')";
 	    $result = mysqli_query($con, $query);
+      echo $result[ "recipe_ID" ]
 	}
 
 ?>
